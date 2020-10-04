e1b814b0b6f8a785b0f79b9ec4690f43383a3d8e
Joshua walker <josh.d.walker@me.com>
2020-10-04 16:50:12 +0100

    update

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,10 +2,17 @@ import { action } from '@storybook/addon-actions';
 
 import Button from './button.svelte';
 
+import LoadingIcon from '@odyssey-dev/loading-icon';
+
 export default {
   title: 'Button',
   component: Button,
 };
+
+
+export const Loading = () => ({
+  Component: LoadingIcon,
+});
 
 export const Text = () => ({
   Component: Button,
