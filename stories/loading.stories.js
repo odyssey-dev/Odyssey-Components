b1e4d110a2a7c4080ab86c2841ec139782d66ba2
Joshua walker <josh.d.walker@me.com>
2020-10-04 17:09:10 +0100

    Components

---
+++ stories/loading.stories.js
@@ -0,0 +1,11 @@
+import LoadingIcon from '@odyssey-dev/loading-icon';
+
+export default {
+  title: 'Loading Icon',
+  component: LoadingIcon,
+};
+
+export const Loading = () => ({
+  Component: LoadingIcon,
+});
+
